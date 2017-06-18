a3edde63c6b59ec43b86205b6a348f316f172288
Yeng <leeyeng2005@gmail.com>
2017-06-17 23:09:30 -0500

    Fix create, add flash & error messages, create show

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 class Article < ActiveRecord::Base
-  validates :title, presence: true
+  validates :title, presence: true, length: { minimum: 3 }
+  validates :description, presence: true, length: { minimum: 10}
 
 
 end

8353cebe03d78ba0366ece4f65e404f3f47b218a
Yeng <leeyeng2005@gmail.com>
2017-06-18 21:24:40 -0500

    Fix sign in upon sign up bug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      session[:user_id] = @user_id
+      session[:user_id] = @user.id
       flash[:success] = "Welcome to the alpha blog #{@user.username}"
       redirect_to user_path(@user)
     else

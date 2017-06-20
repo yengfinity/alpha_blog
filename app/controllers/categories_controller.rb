53376b45acb94a6e4cc40e3fad0da1ef28f2abfc
Yeng <leeyeng2005@gmail.com>
2017-06-19 22:21:35 -0500

    Update view and edit categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -19,8 +19,23 @@ class CategoriesController < ApplicationController
     end
   end
 
-  def show
+  def edit
+    @category = Category.find(params[:id])
+  end
 
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      flash[:success] = "Category name was successfully updated"
+      redirect_to categories_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @category = Category.find(params[:id])
+    @category_articles = @category.articles.paginate(page: params[:page], per_page: 5)
   end
 
   private

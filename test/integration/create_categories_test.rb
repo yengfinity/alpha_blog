baa63dc964cd3c6b4aa4ffa2516fcbe3621d5ce7
Yeng <leeyeng2005@gmail.com>
2017-06-19 19:51:36 -0500

    Add index listing test

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -13,7 +13,6 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
   end
 
   test "invalid category submission results in failure" do
-
     get new_category_path
     assert_template 'categories/new'
     assert_no_difference 'Category.count' do
@@ -21,8 +20,7 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
     end
     assert_template 'categories/new'
     assert_select 'h2.panel-title'
-    assert_select 'div.pane-body'
-
+    assert_select 'div.panel-body'
   end
 
 

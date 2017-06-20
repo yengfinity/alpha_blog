3de09f87581efb46c37091d6946713caf031730d
Yeng <leeyeng2005@gmail.com>
2017-06-19 22:47:47 -0500

    Homework, add integration test to user sign up & article creation

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -7,7 +7,7 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
   end
 
   test "get new category form and create category" do
-    # sign_in_as(@user, "password")
+    sign_in_as(@user, "password")
     get new_category_path
     assert_template 'categories/new'
     assert_difference 'Category.count', 1 do
@@ -18,7 +18,7 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
   end
 
   test "invalid category submission results in failure" do
-    # sign_in_as(@user, "password")
+    sign_in_as(@user, "password")
     get new_category_path
     assert_template 'categories/new'
     assert_no_difference 'Category.count' do
